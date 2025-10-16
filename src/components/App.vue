e05945590feef5f8fe00859cc54f797914126990
Treachery <CreatureOfSilliness@proton.me>
2025-10-16 22:19:03 +0200

    initial commit

---
+++ src/components/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <p>Work in progress...</p>
+        <p>Rendered {{new Date(Date.now()).toLocaleString()}}</p>
+    </div>
+</template>
